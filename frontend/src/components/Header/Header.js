ccbc8c587c8e1427817e969f9b01fd08cab837ae
harrsch2 <harrsch2@illinois.edu>
2020-10-31 16:29:26 -0500

    Add header for frontend

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <div className = "Header">
+      <h1>Stock Hindsight</h1>
+    </div>
+  )
+}
+
+
+export default Header;
