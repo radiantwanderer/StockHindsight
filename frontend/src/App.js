834f6bba2b567eff2f0c49c47e28bb66d167b613
radiantwanderer <radiantgamewanderer@gmail.com>
2020-11-06 14:14:07 -0600

    Implement react-router and bootstrap navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { Switch, Route, Link } from "react-router-dom";
 import Header from './components/Header/Header';
-//import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tutorials" className="navbar-brand">
+            StockHindsight
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/stocks_list"} className="nav-link">
+                Stocks List
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add_stocks"} className="nav-link">
+                Add Stocks
+              </Link>
+            </li>
+          </div>
+        </nav>
+      </div>
   );
 }
 
